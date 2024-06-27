671dfd0f798c8556ded2102ffbce11b6c934a030
AserCenter <aser0bc@gmail.com>
2024-06-27 14:07:12 +0300

    operations

--- js/index.js
+++ js/index.js
@@ -1,8 +1,28 @@
 // declare تعريف المتغبرات
 // assign اعطاءه قيمة
 
+
+
+
 let name = "haia";
 let sum;
-console.log(name);
+let des;
 
+
+let x =5;
+let y = 3;
+sum = x + y;
 console.log(sum);
+
+des = x - y;
+console.log(des);
+
+
+
+ let multipy= x * y;
+console.log(multipy);
+
+let eq= x / y;
+console.log(eq);
+
+
